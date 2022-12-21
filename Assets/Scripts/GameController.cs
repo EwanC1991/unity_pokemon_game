5a5ae28b9be40a599a30c8d6cc885bfcd0b14fe8
Mr Ewan Cruickshank <ewancruickshank@Ewan-Cruickshanks-iMac.local>
2022-12-21 10:17:38 +0000

    Fixed some bugs with scene switching and created enums to link different portals to different locations in alternative scenes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GameState { FreeRoam, Battle, Dialog, Cutscene }
+public enum GameState { FreeRoam, Battle, Dialog, Cutscene, Paused }
 public class GameController : MonoBehaviour
 {
     [SerializeField] PlayerController playerController;
     [SerializeField] BattleSystem battleSystem;
     [SerializeField] Camera worldCamera;
    GameState state;
+
+   GameState stateBeforePause;
 
    public static GameController Instance;
 
@@ -33,6 +35,19 @@ public class GameController : MonoBehaviour
           if (state == GameState.Dialog)
                state = GameState.FreeRoam;
         };
+   }
+
+   public void PauseGame(bool pause)
+   {
+        if (pause)
+        {
+            stateBeforePause = state;
+            state = GameState.Paused;
+        }
+        else 
+        {
+            state = stateBeforePause;
+        }
    }
 
    public void StartBattle() 
--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -8,6 +8,7 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
 {
 
     [SerializeField] int sceneToLoad = -1;
+    [SerializeField] DestinationIdentifier destinationPortal;
     [SerializeField] Transform spawnPoint;
 
     PlayerController player;
@@ -21,13 +22,20 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
     {
         DontDestroyOnLoad(gameObject);
 
+        GameController.Instance.PauseGame(true);
+
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
-        var destPortal = FindObjectsOfType<Portal>().First(x => x != this);
+        var destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
 
         player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
+
+        GameController.Instance.PauseGame(false);
+
         Destroy(gameObject);
     }
 
     public Transform SpawnPoint => spawnPoint;
 }
+
+public enum DestinationIdentifier { A, B, C, D, E, F}
