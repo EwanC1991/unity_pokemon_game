7af3fc4b7e062bc30a154155e4d001e5bcf64763
Mr Ewan Cruickshank <ewancruickshank@Ewan-Cruickshanks-iMac.local>
2022-12-21 09:45:01 +0000

    Added logic and spawn points for scene switching and tested

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
+
+    [SerializeField] int sceneToLoad = -1;
+    [SerializeField] Transform spawnPoint;
+
+    PlayerController player;
     public void OnPlayerTriggered(PlayerController player)
     {
-        Debug.Log("Player entered the portal");
+        this.player = player;
+        StartCoroutine(SwitchScene());
     }
+
+    IEnumerator SwitchScene()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+        var destPortal = FindObjectsOfType<Portal>().First(x => x != this);
+
+        player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
+        Destroy(gameObject);
+    }
+
+    public Transform SpawnPoint => spawnPoint;
 }
