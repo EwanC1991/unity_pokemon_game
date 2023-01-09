505a6a26c10e6fb6fa8206461d9b5a630012daac
Mr Ewan Cruickshank <ewancruickshank@Ewan-Cruickshanks-iMac.local>
2023-01-09 13:29:15 +0000

    Added Items as ScriptableObjects and now the Inventory UI updates to what items are in a players inventory

---
+++ Assets/Scripts/Inventory/UI/InventoryUI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField] GameObject itemsList;
+    [SerializeField] ItemSlotUI itemSlotUI;
+
+    [SerializeField] Image itemIcon;
+    [SerializeField] Text itemDescription;
+
+    int selectedItem = 0;
+
+
+    List<ItemSlotUI> slotUIList;
+    Inventory inventory;
+
+    private void Awake() 
+    {
+        inventory = Inventory.GetInventory();
+    }
+
+    private void Start() 
+    {
+        UpdateItemList();
+    }
+
+    void UpdateItemList()
+    {
+        // Clear all existing items
+        foreach (Transform child in itemsList.transform)
+            Destroy(child.gameObject);
+
+        slotUIList = new List<ItemSlotUI>();
+
+        foreach (var itemSlot in inventory.Slots)
+        {
+            var slotUIObj = Instantiate(itemSlotUI, itemsList.transform);
+            slotUIObj.SetData(itemSlot);
+
+            slotUIList.Add(slotUIObj);
+        }
+
+        UpdateItemSelection();
+
+    }
+    public void HandleUpdate(Action onBack)
+    {
+
+        int prevSelection = selectedItem;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            ++selectedItem;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            --selectedItem;
+
+        selectedItem = Mathf.Clamp(selectedItem, 0, inventory.Slots.Count - 1);
+
+        if (prevSelection != selectedItem)
+            UpdateItemSelection();
+
+        if (Input.GetKeyDown(KeyCode.X))
+            onBack?.Invoke();
+    }
+
+    void UpdateItemSelection()
+    {
+        for (int i = 0; i < slotUIList.Count; i++)
+        {
+            if (i == selectedItem)
+                slotUIList[i].NameText.color = GlobalSettings.i.HighlightedColor;
+            else
+                slotUIList[i].NameText.color = Color.black;
+        }
+
+        var item = inventory.Slots[selectedItem].Item;
+        itemIcon.sprite = item.Icon;
+        itemDescription.text = item.Description;
+    }
+}
